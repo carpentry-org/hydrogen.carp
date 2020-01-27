986c4d70cf47a1e7ce6792c68c5c4154e8726011
hellerve <veit@veitheller.de>
2020-01-27 18:05:53 +0100

    kx: make it compile again

--- hydrogenbuf.h
+++ hydrogenbuf.h
@@ -44,14 +44,28 @@ hydro_kx_session_keypair HydroKX_session_MINUS_keypair() {
   return pair;
 }
 
+hydro_kx_state HydroKX_state() {
+  hydro_kx_state state;
+  return state;
+}
+
 uint8_t* HydroKX_pk(hydro_kx_keypair* pair) {
   return pair->pk;
 }
 
-uint8_t* HydroSign_tx(hydro_session_keypair* pair) {
-  return pair->tx;
+Array Hydro_make_x_array(uint8_t e[hydro_kx_SESSIONKEYBYTES]) {
+  Array res;
+  res.len = hydro_kx_SESSIONKEYBYTES;
+  res.capacity = hydro_kx_SESSIONKEYBYTES;
+  res.data = CARP_MALLOC(hydro_kx_SESSIONKEYBYTES);
+  memcpy(res.data, e, hydro_kx_SESSIONKEYBYTES);
+  return res;
 }
 
-uint8_t* HydroSign_rx(hydro_session_keypair* pair) {
-  return pair->rx;
+Array HydroSign_tx(hydro_kx_session_keypair* pair) {
+  return Hydro_make_x_array(pair->tx);
+}
+
+Array HydroSign_rx(hydro_kx_session_keypair* pair) {
+  return Hydro_make_x_array(pair->rx);
 }
